8b03cb425c43484c48df91e88c786c5803c42dd8
Victor Basumatary <vlenceleuth@gmail.com>
2025-07-18 16:27:37 +0530

    Add fuzz test for timer

--- timer_test.go
+++ timer_test.go
@@ -1,6 +1,7 @@
 package simtest
 
 import (
+	"math/rand/v2"
 	"testing"
 	"time"
 )
@@ -72,4 +73,50 @@ func TestTimerFiredOnlyOnce(t *testing.T) {
         if !fired {
                 t.Errorf("timer not fired")
         }
+}
+
+func FuzzTestTimerFiredOnlyOnce(f *testing.F) {
+        minResolution := int64(time.Microsecond)
+        maxResolution := int64(time.Second)
+
+        for range 10 {
+                mul := rand.Int64N(9) + 1
+                res := rand.Int64N(maxResolution - minResolution) + minResolution
+                tickSize := mul * res
+
+                f.Add(tickSize)
+        }
+
+        f.Fuzz(func(t *testing.T, a int64) {
+                epoch := time.Now()
+                clock := NewSimClock(epoch)
+                defer clock.Stop()
+
+                tickSize := time.Duration(a)
+                durMul := rand.Int64N(8) + 2 // duration is 2-10x the tickSize
+                dur := time.Duration(durMul * a)
+
+                minTicks := dur / tickSize // min number of ticks that will be required to fire the timer
+                maxTicks := minTicks + 100 // max iterations we are willing to wait
+
+                _, ch := clock.NewTimer(dur)
+
+                fired := false
+                for range maxTicks {
+                        select {
+                        case <-ch:
+                                if fired {
+                                        t.Errorf("timer fired twice")
+                                }
+
+                                fired = true
+                        default:
+                                clock.Tick(tickSize)
+                        }
+                }
+
+                if !fired {
+                        t.Errorf("timer not fired")
+                }
+        })
 }
